10aa66280ff9d68468699b89acd40948dcb2a595
CanLi <Roberick@qq.com>
2025-02-10 15:08:52 +0800

    fix(reply): fix issue with handling multiple target labels

--- reply_module/reply.py
+++ reply_module/reply.py
@@ -4,11 +4,11 @@ from reply_module.reply_target.reply_factory import ReplyFactory
 
 
 class Reply:
-    def __init__(self, project_id, merge_request_id):
+    def __init__(self, project_id, merge_request_iid):
         self.replies = []
         self.lock = threading.Lock()
         self.project_id = project_id
-        self.merge_request_id = merge_request_id
+        self.merge_request_iid = merge_request_iid
 
     def add_reply(self, reply):
         # reply 格式检查：title, content 必选
@@ -25,21 +25,35 @@ class Reply:
             self.replies.append(reply)
 
     def send(self):
-        markdown_message = ""
+        msg_list = {}
         with self.lock:  # 加锁
             # 发送所有消息的逻辑
             for reply in self.replies:
-                markdown_message += f"## {reply['title']}\n\n{reply['content']}\n\n"
+                targets = [t.strip() for t in reply['target'].split(',')]
+                if 'all' in targets:
+                    targets = ReplyFactory.get_all_targets()
+                for target in targets:
+                    msg_list[target] = msg_list.get(target, '')
+                    msg_list[target] += f"## {reply['title']}\n\n{reply['content']}\n\n"
             self.replies = []  # 清空已发送的消息
-        reply_target = ReplyFactory.get_reply_instance(reply['target'], self.project_id, self.merge_request_id)
-        return reply_target.send(markdown_message)
+        ret = True
+        for target, msg in msg_list.items():
+            reply_target = ReplyFactory.get_reply_instance(target, self.project_id, self.merge_request_iid)
+            ret &= reply_target.send(msg)
+        return ret
 
     def send_single_message(self, reply):
         """
         实时发送消息
         """
-        reply_target = ReplyFactory.get_reply_instance(reply['target'], self.project_id, self.merge_request_id)
-        return reply_target.send(reply['content'])
+        targets = [t.strip() for t in reply['target'].split(',')]
+        if 'all' in targets:
+            targets = ReplyFactory.get_all_targets()
+        ret = True
+        for target in targets:
+            reply_target = ReplyFactory.get_reply_instance(target, self.project_id, self.merge_request_iid)
+            ret &= reply_target.send(f"## {reply['title']}\n\n{reply['content']}\n\n")
+        return ret
 
 
 if __name__ == '__main__':
@@ -47,7 +61,8 @@ if __name__ == '__main__':
     threads = []
     for i in range(10):
         threads.append(threading.Thread(target=reply.add_reply, args=(
-            {'title': f'title{i}', 'content': f'content{i}', 'target': 'gitlab', 'priority': i % 3},)))
+            {'title': f'title{i}', 'content': f'content{i}', 'target': 'gitlab, dingtalk', 'priority': i % 3},)))
+
     for thread in threads:
         thread.start()
     for thread in threads:
--- reply_module/reply_target/reply_factory.py
+++ reply_module/reply_target/reply_factory.py
@@ -1,13 +1,28 @@
 from reply_module.reply_target.dingtalk_reply import DingtalkReply
 from reply_module.reply_target.gitlab_reply import GitlabReply
 
-
 class ReplyFactory:
-    @staticmethod
-    def get_reply_instance(target, project_id, merge_request_id):
-        if target == 'gitlab':
-            return GitlabReply(project_id, merge_request_id)
-        elif target == 'dingtalk':
-            return DingtalkReply(project_id, merge_request_id)
-        else:
+    _registry = {}
+
+    @classmethod
+    def register_target(cls, target, target_class):
+        cls._registry[target] = target_class
+
+    @classmethod
+    def get_reply_instance(cls, target, project_id, merge_request_id):
+        if target not in cls._registry:
             raise ValueError(f"Unknown target: {target}")
+        return cls._registry[target](project_id, merge_request_id)
+
+    @classmethod
+    def get_all_reply_instance(cls, project_id, merge_request_id):
+        return [target_class(project_id, merge_request_id) for target_class in cls._registry.values()]
+
+    @classmethod
+    def get_all_targets(cls):
+        return list(cls._registry.keys())
+
+
+
+ReplyFactory.register_target('gitlab', GitlabReply)
+ReplyFactory.register_target('dingtalk', DingtalkReply)
