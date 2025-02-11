8b83d2062edff4da01b3d69eea0a1b9994c3d39b
CanLi <Roberick@qq.com>
2025-02-11 16:55:22 +0800

    chore: remove unused files

--- service/content_handle.py
+++
@@ -1,5 +0,0 @@
-import re
-def filter_diff_content(diff_content):
-    filtered_content = re.sub(r'(^-.*\n)|(^@@.*\n)', '', diff_content, flags=re.MULTILINE)
-    processed_code = '\n'.join([line[1:] if line.startswith('+') else line for line in filtered_content.split('\n')])
-    return processed_code
